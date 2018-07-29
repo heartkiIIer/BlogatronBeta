9c714fc3a4f24ae3aae54a48320edc205d056c7f
7-Zip <28637164+7-Zip@users.noreply.github.com>
2018-07-28 21:23:28 -0400

    Add routes and forms to enable users to edit and update comments

--- routes/comments.js
+++ routes/comments.js
@@ -45,6 +45,28 @@ router.post("/", isLoggedIn, (req, res) => {
   });
 });
 
+// Show form for editing comment
+router.get("/:comment_id/edit", (req, res) => {
+  Comment.findById(req.params.comment_id, (err, foundComment) => {
+    if(err) {
+      res.redirect("back");
+    } else {
+      res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+    }
+  })
+});
+
+// Update campground with given data
+router.put("/:comment_id", (req,res) => {
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
+    if(err) {
+      res.redirect("back");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
+});
+
 // Middleware to check if user is logged in
 function isLoggedIn(req, res, next) {
     if(req.isAuthenticated()) {
