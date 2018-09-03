4432ed8b25bde4e6a857827956e0091c6af47496
Matt <mattkorn129@gmail.com>
2018-09-02 20:06:31 -0400

    Add environment variables for remote database

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp";
+
+mongoose.connect(url, { useNewUrlParser: true });
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -51,6 +54,6 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, process.env.IP, () => {
 	console.log("The YelpCamp server is listening on port 3000...");
 });
