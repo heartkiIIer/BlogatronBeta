eb3103fa73734b48ce081c48f7a22e533a0dfb88
Glitch (7-zip-yelpcamp) <none>
2019-09-15 13:04:05 +0000

    🍦🍢 Checkpoint ./package.json:6371005/44 ./app.js:6371005/14

--- app.js
+++ app.js
@@ -62,6 +62,6 @@ app.use((req, res, next) => {
 app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
-app.listen(process.env.PORT || 3000, process.env.IP, () => {
+app.listen(process.env.PORT, process.env.IP, () => {
 	console.log("The YelpCamp server is listening on port 3000...");
 });
